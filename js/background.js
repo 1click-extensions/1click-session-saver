ea40c05dba31cb135e2a43d8b15c042bc8ef3195
Itsick <any.site.n.site@gmail.com>
2018-06-01 14:22:30 +0300

    working

---
+++ js/background.js
@@ -0,0 +1,94 @@
+function fixNumber(num){
+	let numAsStr = '' + num;
+	if(numAsStr.length = 1){
+		numAsStr = '0' + numAsStr;
+	} 
+	return numAsStr;
+}
+
+chrome.runtime.setUninstallURL("https://1ce.org");
+
+if (!localStorage.created) {
+  chrome.tabs.create({ url: "https://1ce.org" });
+  var manifest = chrome.runtime.getManifest();
+  localStorage.ver = manifest.version;
+  localStorage.created = 1;
+}
+
+SessionsStore = {
+	getAllSessions : function(){
+		return SessionsStore.getJson('sessions');
+	},
+	addSession: function(item){
+		items = SessionsStore.getAllSessions();
+		//console.log('items', items);
+		items[item.time] = item;
+		SessionsStore.storeJson('sessions',items);
+	},
+	storeJson : function(name, data){
+        localStorage.setItem(name, JSON.stringify(data));
+    },
+    getJson : function(name){
+        let data = '';
+        try{
+            data = JSON.parse(localStorage.getItem(name));
+        }
+        catch(e){
+
+        }
+        return data ? data : {};
+    },
+}
+
+chrome.browserAction.onClicked.addListener(function(tab) { 
+    chrome.tabs.create({url:chrome.extension.getURL('pages/notepad.html')})  
+});
+
+chrome.runtime.onMessage.addListener( function(request,sender,sendResponse)
+{
+    if( request.action === "getSessions" )
+    {
+        sendResponse(SessionsStore.getAllSessions());      
+    }
+    if( request.action === "openSession" )
+    {
+        let sessions = SessionsStore.getAllSessions(),
+        	selectedSession = sessions[request.time];
+
+        if(selectedSession){
+        	//console.log(selectedSession,sessions);
+        	for(let tab of selectedSession.tabs){
+        		//console.log(tab);
+        		if(tab){
+        			chrome.tabs.create({ url:tab.url});
+        		}
+        	}
+        }
+        sendResponse();      
+    }
+    if( request.action === "addSession" )
+    {
+        chrome.tabs.query({currentWindow: true}, function(tabs) {
+		    var currTime = new Date(),
+		    	currentSession = {
+			    	time : [fixNumber(currTime.getDate()), fixNumber(currTime.getMonth()) + 1 , currTime.getFullYear()].join('/') + ' ' + currTime.getHours() + ':' + currTime.getMinutes(),
+			    	tabs : []
+			    };
+		    tabs.forEach(function(tab) {
+		        currentSession.tabs.push({
+		        	title: tab.title,
+		        	url:tab.url
+		        });
+		    });
+		    SessionsStore.addSession(currentSession);
+		});     
+    }
+    if( request.action === "removeSession" )
+    {
+    	let sessions = SessionsStore.getAllSessions();
+        delete(sessions[request.time]);
+        SessionsStore.storeJson('sessions', sessions);
+        sendResponse(true);
+
+    }
+});
---
+++ js/popup.js
@@ -0,0 +1,54 @@
+$(function(){
+    chrome.runtime.sendMessage({action: "getSessions"},
+        function (response) {
+            //console.log(response);
+            for(let sessionId in response){
+                let session = response[sessionId],
+                    sessShow = $('<div class="session-show">');
+                sessShow.append('<span class="name">' + session.time + '</span>');
+                sessShow.append('<button class="open">'+chrome.i18n.getMessage('load')+'</button>');
+                sessShow.append('<button class="remove">'+chrome.i18n.getMessage('remove')+'</button>');
+                sessShow.append('<ul class="tabs-list"></ul>');
+                let list = sessShow.find('.tabs-list');
+                for(let tab of session.tabs){
+                    list.append('<li>' + tab.title.substr(0,22) + '</li>');
+                }
+                sessShow.find('.open').click(function(){
+                    chrome.runtime.sendMessage({action: "openSession",time: session.time});
+                });
+                sessShow.find('.remove').click(function(){
+                    //let li = $(this).closest('li');
+                    chrome.runtime.sendMessage({action: "removeSession",time: session.time},{}, function(response){
+                        //console.log(response);
+                        if(response){
+                            //console.log('li', sessShow); 
+                            sessShow.remove();
+                        }
+                    });
+
+                });
+                $('.recent-sessions').append(sessShow);
+            }
+        });
+
+    $('.recent').text(chrome.i18n.getMessage('recent_title'))
+    $('#new-session').click(function(){
+         chrome.permissions.contains({
+            permissions: ['tabs']},function(status){
+                if(status){
+                    chrome.runtime.sendMessage({action: "addSession"});
+                }
+                else{
+                    chrome.permissions.request({
+                        permissions: ['tabs']},function(status){
+                            if(status){
+                                chrome.runtime.sendMessage({action: "addSession"});
+                            }
+                            else{
+                                alert('Cant save session without permissions')
+                            }
+                    });
+                }
+            });
+        });
+});
